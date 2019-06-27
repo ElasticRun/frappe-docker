7c9ceba357861ca610f0fc69d8520dd274624086
Nikhil Sharma <nikhil.sharma@elastic.run>
2019-06-27 14:15:23 +0530

    fix: updated further for naming conventions and external service names.

--- entrypoints/10_mkdirs.sh
+++ entrypoints/10_mkdirs.sh
@@ -1,9 +1,55 @@
 #!/bin/sh
 export SITE=${SITE_NAME:-site1.docker}
-sudo cp -R /home/frappe/sites-backup/.build ${BENCH_HOME}/sites/ \
-     && sudo cp -R /home/frappe/sites-backup/* ${BENCH_HOME}/sites/ \
-     && sudo ln -s ${BENCH_HOME}/apps/frappe/frappe/public ${BENCH_HOME}/sites/assets/frappe \
-     && sudo rm -rf /home/frappe/sites-backup
+mkdir -p ${BENCH_HOME}/sites_orig/assets
+# Copy original sites data into sites_orig
+export BASE=${BENCH_HOME}/sites
+export NEW_BASE=${BENCH_HOME}/sites_orig
+# Backup of contents of sites
+for d in `ls ${BASE}`
+do
+    if [ "$d" != "assets" -a -L ${BASE}/$d ]
+    then
+        mv ${BASE}/$d ${NEW_BASE}/$d
+    fi
+done
+# Backup of assets
+for d in `ls ${BASE}/assets`
+do
+    # Ignore symlinks from earlier
+    if [ ! -L ${BASE}/assets/$d ]
+    then
+        mv -f ${BASE}/assets/$d ${NEW_BASE}/assets/$d
+    fi
+done
+# Remove original data from sites
+rm -rf ${BENCH_HOME}/sites/*
+
+# Install new data from sites-backup.
+export BACKUP_BASE=/home/frappe/sites-backup
+export NEW_BASE=${BENCH_HOME}/sites
+mkdir -p ${NEW_BASE}/assets
+for d in `ls ${BACKUP_BASE}`
+do
+    if [ "$d" != "assets" -a -L ${BACKUP_BASE}/$d ]
+    then
+        mv ${BACKUP_BASE}/$d ${NEW_BASE}/$d
+    fi
+done
+# handle assets separately
+for d in `ls ${BACKUP_BASE}/assets`
+do
+    if [ -L ${BACKUP_BASE}/assets/$d ]
+    then
+        # Handle sym link
+        echo "${BACKUP_BASE}/assets/$d is symlink"
+        target=`readlink -f ${BACKUP_BASE}/assets/$d`
+        ln -s $target ${NEW_BASE}/assets/$d
+    else
+        mv -f ${BACKUP_BASE}/assets/$d ${NEW_BASE}/assets/$d
+    fi
+done
+
+sudo rm -rf ${BACKUP_BASE}
 
 sudo chown -R frappe:frappe ${BENCH_HOME}/sites ${BENCH_HOME}/logs
 if [ ! -f ${BENCH_HOME}/sites/apps.txt ]
--- entrypoints/30_setup_site.sh
+++ entrypoints/30_setup_site.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
+BENCH_HOME=/home/frappe/${BENCH_NAME}
 cd ${BENCH_HOME}
 sudo chown -R frappe:frappe ${BENCH_HOME}/sites
 if [ ! -d ${BENCH_HOME}/sites/${SITE} -o ! -f ${BENCH_HOME}/sites/${SITE}/site_config.json ]
 then
+    echo "Deleting existing user, if any..."
+    mysql -h ${DB_HOST} -u root -p${DB_PASSWORD} -e "delete from user where user = '${DB_NAME}'; commit; flush privileges;" mysql
+    # echo "Grant all privileges to root@%..."
+    # mysql -h ${DB_HOST} -u root -p${DB_PASSWORD} -e \
+    #         "grant all privileges on *.* to root@\`%\` identified by '${DB_PASSWORD}'; commit; flush privileges;" mysql
     echo "creating new site ${SITE}"
     bench new-site --force --db-name ${DB_NAME} --mariadb-root-username root --admin-password ${ADMIN_PASSWORD} --verbose ${SITE}
-    echo -n "${SITE}" > ${BENCH_HOME}/sites/currentsite.txt
+    STATUS=$?
+    if [ $STATUS -ne 0 ]
+    then
+        echo "ERROR: An error occurred while setting up new site. Please review logs, correct the error and retry."
+        false
+    else
+        echo "Providing privileges to new user."
+        mysql -h ${DB_HOST} -u root -p${DB_PASSWORD} -e \
+            "grant all privileges on \`${DB_NAME}\`.* to \`${DB_NAME}\`@\`%\` identified by '${DB_PASSWORD}'; commit; flush privileges;" mysql
+        mysql -h ${DB_HOST} -u root -p${DB_PASSWORD} -e \
+            "delete from user where user = '${DB_NAME}' and host <> '%'; commit; flush privileges;" mysql
+        bench set-config db_password ${DB_PASSWORD}
+        echo -n "${SITE}" > ${BENCH_HOME}/sites/currentsite.txt
+    fi
 fi
