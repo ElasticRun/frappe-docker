734c5f5c54a292db59866aacb17a07c2d22bff22
Nikhil Sharma <nikhil.sharma@elastic.run>
2019-07-23 08:27:39 +0530

    fix: copying of common-site-config to sites folder.

--- entrypoints/10_mkdirs.sh
+++ entrypoints/10_mkdirs.sh
@@ -4,7 +4,7 @@ export SITE=${SITE_NAME:-site1.docker}
 #      && sudo cp -R /home/frappe/sites-backup/* ${BENCH_HOME}/sites/ \
 #      && sudo ln -s ${BENCH_HOME}/apps/frappe/frappe/public ${BENCH_HOME}/sites/assets/frappe \
 #      && sudo rm -rf /home/frappe/sites-backup
-
+cp /home/frappe/common_site_config.json ${BENCH_HOME}/sites/common_site_config.json
 sudo chown -R frappe:frappe ${BENCH_HOME}/sites ${BENCH_HOME}/logs
 if [ ! -f ${BENCH_HOME}/sites/apps.txt ]
 then
