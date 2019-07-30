20ce1003b4374bba8fea70f39c7dff1766036ec1
Ajit P <ajit.pendse@elastic.run>
2019-07-30 22:10:55 +0530

    fix:more debug

--- build-frappe10.sh
+++ build-frappe10.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 TAG=$1
 # Uses default frappe and bench master branches from github to build the docker image. Use own URLs to override.
-echo "Kafka Config : ${KAFKA_CONFIG}"
-docker build -f Dockerfile_frappe10 --build-arg KAFKA_CONFIG=${KAFKA_CONFIG} \
+echo "Kafka Config : '${KAFKA_CONFIG}'"
+docker build -f Dockerfile_frappe10 --build-arg KAFKA_CONFIG='${KAFKA_CONFIG}' \
   --build-arg GIT_FRAPPE_URL=github.com/frappe/frappe.git --build-arg BENCH_BRANCH=v3.x --build-arg GIT_BENCH_URL=github.com/frappe/bench.git \
   -t dock.elasticrun.in/er-frappe10-base:${TAG} .
   #GIT_FRAPPE_URL=engg.elasticrun.in/tredrun/tredrun-core/frappe.git
