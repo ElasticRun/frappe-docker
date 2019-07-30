1af208a9d5a0afa5db50e5173fb1b4bb47691a8d
Ajit P <ajit.pendse@elastic.run>
2019-07-30 22:08:26 +0530

    feat: added kafka_config as part of build

--- build-frappe11.sh
+++ build-frappe11.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 TAG=$1
 # Uses default frappe and bench master branches from github to build the docker image. Use own URLs to override.
-docker build \
+docker build --build-arg KAFKA_CONFIG=${KAFKA_CONFIG} \
   --build-arg GIT_FRAPPE_URL=github.com/frappe/frappe.git --build-arg FRAPPE_BRANCH=version-11 --build-arg GIT_BENCH_URL=github.com/frappe/bench.git \
   -t dock.elasticrun.in/er-frappe11-base:${TAG} .
