e2c6cb8571ebb3988d41c51b75e8d4d640b9db56
Ajit P <ajit.pendse@elastic.run>
2019-08-02 12:46:42 +0530

    fix:entrypoint existing .lock file

--- entrypoints/10_mkdirs.sh
+++ entrypoints/10_mkdirs.sh
@@ -19,7 +19,8 @@ then
     sudo rm -rf ${BENCH_HOME}/sites/currentsite.txt
 fi
 
-cp /home/frappe/common_site_config_docker.json ${BENCH_HOME}/sites/common_site_config.json
+# No longer required as sites directory is now within the image.
+#cp /home/frappe/common_site_config_docker.json ${BENCH_HOME}/sites/common_site_config.json
 sudo chown -R frappe:frappe ${BENCH_HOME}/sites ${BENCH_HOME}/logs
 if [ ! -f ${BENCH_HOME}/sites/apps.txt ]
 then
