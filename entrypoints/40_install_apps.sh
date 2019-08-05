9fc34e7cbe1783720db66ffcb4630aa35e7fee77
Ajit P <ajit.pendse@elastic.run>
2019-08-05 18:07:40 +0530

    fix: moved to bash shell

--- entrypoints/40_install_apps.sh
+++ entrypoints/40_install_apps.sh
@@ -1,8 +1,8 @@
 cd ${BENCH_HOME}
 SITE=`cat ${BENCH_HOME}/sites/currentsite.txt`
-INSTALLED_APPS=`bench list-apps`
-INSTALLED_APPS_COUNT=`echo $INSTALLED_APPS | wc -l`
-APPS_DIR_COUNT=`ls -l ./apps | wc -l`
+export INSTALLED_APPS=`bench list-apps`
+export INSTALLED_APPS_COUNT=`echo $INSTALLED_APPS | wc -l`
+export APPS_DIR_COUNT=`ls -l ./apps | wc -l`
 echo "Installed Apps - ${INSTALLED_APPS}(${INSTALLED_APPS_COUNT})"
 echo "Apps Dir Count - ${APPS_DIR_COUNT}"
 if [ ${APPS_DIR_COUNT} -ne ${INSTALLED_APPS_COUNT} ]
@@ -16,23 +16,22 @@ then
         # Install only if not available.
         echo "Processing application $app"
         export VAL=$app
-        sudo echo "$app" >> ${BENCH_HOME}/sites/apps.txt
-        awk 'BEGIN
-            {
-                liststr = ENVIRON["INSTALLED_APPS"]
-                val = ENVIRON["VAL"]
-                #print "val = ", val
-                arrlen = split(liststr, APP_ARR, sep="\n")
-                #print "APP_ARR = ", APP_ARR[1], " length = ", arrlen
-                retval = 1
-                i = 1
-                while ( and(i <= arrlen, retval == 1) ) {  printf("%i - key - %s\n", i, APP_ARR[i]);  if (APP_ARR[i]==val) {retval = 0}; i += 1 }
-                #print "retval - ", retval
-                exit retval
-            }
-            END
-            { exit retval }'
-        if [ $? -ne 0 ]
+        echo "Exported VAL=$VAL. Adding to apps.txt"
+        echo "$app" >> ${BENCH_HOME}/sites/apps.txt
+        AWK_RESULT=$(awk -e 'BEGIN {
+            liststr = ENVIRON["INSTALLED_APPS"]
+            val = ENVIRON["VAL"]
+            print "val = ", val
+            arrlen = split(liststr, APP_ARR, sep="\n")
+            print "APP_ARR = ", APP_ARR[1], " length = ", arrlen
+            retval = 1
+            i = 1
+            while ( and(i <= arrlen, retval == 1) ) {  printf("%i - key - %s\n", i, APP_ARR[i]);  if (APP_ARR[i]==val) {retval = 0}; i += 1 }
+            print "retval - ", retval
+            print retval
+        }')
+        echo "AWK Result = ${AWK_RESULT}"
+        if [ ${AWK_RESULT} -ne 0 ]
         then
             echo "Installing Application '$app'"
             bench --site ${SITE} install-app $app
