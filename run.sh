f019759289c99ca1179e4e3e2c31fdf020de6fbc
Ajit P <ajit.pendse@elastic.run>
2019-09-16 10:11:23 +0530

    fix: supervisor config and log file tail updated

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ BENCH_HOME=/home/frappe/${BENCH_NAME}
 cd ${BENCH_HOME}
 SUCCESS=0
 
+export GUNI_WORKER_CONNECTIONS=${GUNI_WORKER_CONNECTIONS:-200}
+export GUNI_WORKERS=${GUNI_WORKERS:-4}
+
 sudo nginx
 # Check if specific worker is specified then start bench with args
 if [ $# -ne 0 ]
@@ -14,6 +17,7 @@ else
     sudo supervisorctl start all >> $BENCH_LOG_FILE 2>&1 &
     BENCH_PID=`echo $!`
 fi
+echo "Started web server with ${GUNI_WORKERS} workers and ${GUNI_WORKER_CONNECTIONS} connections per worker."
 
 echo $BENCH_PID > $BENCH_HOME/${BENCH_NAME}.pid
 echo "Bench started with Process ID - ${BENCH_PID}"
@@ -40,5 +44,9 @@ then
     echo "One of the post boot scripts failed... Please check logs for more information"
 fi
 
-tail -F /home/frappe/docker-bench/logs/console.log -F /home/frappe/docker-bench/logs/access.log \
-    -F /home/frappe/docker-bench/logs/bench.log -F /home/frappe/docker-bench/logs/frappe.log
+TAIL_CMD="tail -F /home/frappe/docker-bench/logs/access.log -F /home/frappe/docker-bench/logs/frappe.log"
+for file in `ls /home/frappe/docker-bench/logs/*.error.log`
+do
+    TAIL_CMD="$TAIL_CMD -F $file"
+done
+exec $TAIL_CMD
